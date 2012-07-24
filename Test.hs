0796c6f8b51345ac4030c9c79c55f63a041b306a
Michal J. Gajda <mjgajda@gmail.com>
2012-07-24 16:15:02 +0200

    Parsing with number conversion.

--- Test.hs
+++ Test.hs
@@ -5,4 +5,4 @@ import Text.CSV.ByteString(parseCSV)
 import System.IO(openFile, IOMode(ReadMode))
 
 main = do Just csv <- parseCSV `fmap` (openFile "K18_alone_shifty.csv" ReadMode >>= BS.hGetContents)
-          print $ csv !! 0
+          print $ csv
