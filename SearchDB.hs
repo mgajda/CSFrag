0332802cbc901a4b35d9789ea770ec6de012cbee
Michal J. Gajda <mgajda@gwdg.de>
2012-08-06 16:22:49 +0200

    SearchDB.hs: added command line help.

--- SearchDB.hs
+++ SearchDB.hs
@@ -1,15 +1,19 @@
-{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings, TypeOperators #-}
 module Main(main) where
 
-import System.Environment(getArgs)
+import System.Environment(getArgs, getProgName)
+import System.Exit
 import Data.Binary
 import Data.List as L
 import Control.Monad(when)
 --import qualified Data.ByteString.Char8 as BS
 
 import qualified Data.Array.Repa as Repa
+import Data.Array.Repa((:.)(..), Z)
 import Database
 import ShiftsCSVInput
+import qualified SeqSim
+import Outer
 
 -- ^ Finding fragments in the database.
 
@@ -24,19 +28,37 @@ shiftIndices queryShiftNames dbShiftNames = (xlate, errs)
     shiftErr (Just i) name = []
     shiftErr Nothing  name = ["No shifts named " ++ name ++ " in database."]
 
+
 -- | Comparing chemical shifts in case names are different, but mean something the same.
 shiftEq "HN" "H" = True
 shiftEq "H" "HN" = True
 shiftEq a   b    = a == b
 
 -- | Fill in index array from assoclist si, and then use it to transfer indices.
-useShiftIndices si db = undefined
---  where
---    shDb = extent . shifts $ db
---    findI (Z :. x :. y) = lookup x si 
+computeScores si query db weights = undefined
+  where
+    shiftsInd i = Repa.slice (csArray db   ) (Repa.Z :. i :. Repa.All)
+    queryInd  i = Repa.slice (shifts  query) (Repa.Z :. i :. Repa.All)
+    --seqScores   = Repa
+
+shiftWeights = [] 
+
+-- | Prints command-line help for the program.
+printUsage = do prog <- getProgName
+                putStrLn $ " " `L.intercalate` usage prog
+ where
+   usage prog = ["Usage: "
+                , prog
+                , "<database from MakeDB>"
+                , "<query.csv>"
+                , "\nProgram for searching with Preditor algorithm."
+                ]
 
 -- | Reads database, and query, then it shows query results (as fragment indices.)
-main = do [dbfname, shiftsfname] <- getArgs
+main = do args <- getArgs
+          when (L.length args /=2) $ do printUsage
+                                        exitFailure
+          let [dbfname, shiftsfname] = args
           (db :: Database) <- decodeCompressedFile dbfname
           let csNum = head . tail . Repa.listOfShape . Repa.extent . csArray $ db
           putStrLn $ L.concat ["Read ", show csNum, " chemical shifts."]
