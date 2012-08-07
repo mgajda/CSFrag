70015ed4b7741568ab69fb31a6930d7e5f57468f
Michal J. Gajda <mjgajda@mail.google.com>
2012-08-07 11:34:28 +0200

    SearchDB: wibble.

--- SearchDB.hs
+++ SearchDB.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE ScopedTypeVariables, OverloadedStrings, TypeOperators #-}
-module Main(main) where
+module Main where
 
 import System.Environment(getArgs, getProgName)
 import System.Exit
 import Data.Binary
 import Data.List as L
 import Control.Monad(when)
---import qualified Data.ByteString.Char8 as BS
+import Control.Exception(assert)
 
 import qualified Data.Array.Repa as Repa
 import Data.Array.Repa((:.)(..), Z)
+import Data.Array.Repa.Eval()
+
 import Database
 import ShiftsCSVInput
 import qualified SeqSim
@@ -21,9 +23,9 @@ import Outer
 shiftIndices queryShiftNames dbShiftNames = (xlate, errs)
   where
     maybeIndices  = map (\n -> L.findIndex (shiftEq n) dbShiftNames) queryShiftNames
-    xlate = concat $ zipWith shiftXlate maybeIndices [1..]
-    shiftXlate (Just i) j = [(i, j)]
-    shiftXlate Nothing  _ = []
+    xlate = concat $ zipWith3 shiftXlate maybeIndices [1..] dbShiftNames
+    shiftXlate (Just i) j name = [(name, i, j)]
+    shiftXlate Nothing  _ name = []
     errs = L.concat $ L.zipWith shiftErr maybeIndices queryShiftNames
     shiftErr (Just i) name = []
     shiftErr Nothing  name = ["No shifts named " ++ name ++ " in database."]
@@ -35,12 +37,21 @@ shiftEq "H" "HN" = True
 shiftEq a   b    = a == b
 
 -- | Fill in index array from assoclist si, and then use it to transfer indices.
-computeScores si query db weights = undefined
+computeScores :: [(String, Int, Int)] -> ShiftsInput -> Database -> (Char -> Char -> Int) -> Repa.Array Repa.D Repa.DIM1 Float
+computeScores si query db seqsim = undefined
   where
     shiftsInd i = Repa.slice (csArray db   ) (Repa.Z :. i :. Repa.All)
     queryInd  i = Repa.slice (shifts  query) (Repa.Z :. i :. Repa.All)
 
-shiftWeights = [] 
+-- | Returns weight of a given chemical shift at a given position within a fragment.
+shiftWeights :: (RealFloat b) => Int -> String -> Maybe b
+shiftWeights relativeIndex name = assert (relativeIndex <= 1 && relativeIndex >= (-1)) $
+                                  L.lookup name (weights !! (relativeIndex + 1))
+  where
+    weights = [[("seqsim", 0.5), ("HA", 37), ("CA", 11), ("CB",  9), ("CO", 5), ("N",1  ), ("H", 1  )] -- -1 on seq
+              ,[("seqsim", 2.5), ("HA", 31), ("CA", 14), ("CB", 14), ("CO", 6), ("N",1.5), ("H", 0.3)] --  0 on seq
+              ,[("seqsim", 1.5), ("HA", 37), ("CA",  7), ("CB",  7), ("CO", 4), ("N",2  ), ("H", 1.5)] -- +1 on seq
+              ]
 
 -- | Prints command-line help for the program.
 printUsage = do prog <- getProgName
@@ -70,6 +81,8 @@ main = do args <- getArgs
           seqSim <-(maybe (error "Cannot find file with sequence similarity weights!") SeqSim.seqSim
                       `fmap` SeqSim.readWeights)
           putStr "Query indices:"
-          print si 
-       
+          print si
+          s <- Repa.computeUnboxedP $ computeScores si input db seqSim
+          print $ (Repa.toList s :: [Float])
+
 
