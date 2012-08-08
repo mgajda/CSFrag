97001cc8877f6ea66875c7c0c487143bf1becc75
Michal J. Gajda <mjgajda@gmail.com>
2012-08-08 11:01:10 +0200

    Added type signatures to SeqSim.

--- SeqSim.hs
+++ SeqSim.hs
@@ -39,6 +39,7 @@ convEntry (rescode:others) =
                     "" -> Right (-1)
                     fs -> Atto.parseOnly Atto.decimal fs
 
+redParse ::  [Either a a1] -> Either a [a1]
 redParse []              = Right []
 redParse (Left msg:_   ) = Left msg
 redParse (Right f :rest) = case redParse rest of
@@ -48,12 +49,14 @@ redParse (Right f :rest) = case redParse rest of
 invIndex :: DIM2 -> DIM2
 invIndex (Z Repa.:. x Repa.:. y) = (Z Repa.:. y Repa.:. x)
 
+symmetrize :: (Eq b, Num b, Repa.Source r b) =>Repa.Array r DIM2 b -> Repa.Array Repa.D DIM2 b
 symmetrize arr = Repa.traverse arr id xform
   where
     xform lookup i = case lookup i of
                        (-1) -> lookup $ invIndex i
                        v    -> v
 
+--mkMatrix :: (Eq b, Eq a, Num b, Data.Vector.Unboxed.Base.Unbox b) => [a] -> [(a, [b])] -> Repa.Array Repa.D DIM2 b
 mkMatrix headers list = assert (map fst list == tail headers ) $
                         assert (squareMatrix . map snd $ list) $
                         symmetrize arr
@@ -67,15 +70,19 @@ computeIndices headers = V.replicate 256 (-1) V.// L.zip ords [0..]
     ords = L.map Data.Char.ord (BS.unpack headers)
 
 -- | TODO: getDefaultWeightsFilename should find data dir!
+getDefaultWeightsFilename ::  IO FilePath
 getDefaultWeightsFilename = return $ "." </> "share" </> "seqsim.txt"
 
+readWeights ::  IO (Maybe SeqSimWeights)
 readWeights = getDefaultWeightsFilename >>= readWeightsFromFile
 
+prepareSeqSim ::  IO (Char -> Char -> Int)
 prepareSeqSim = (maybe errMsg SeqSim.seqSim
                    `fmap` SeqSim.readWeights)
   where
     errMsg = error "Cannot find file with sequence similarity weights!"
 
+readWeightsFromFile ::  FilePath -> IO (Maybe SeqSimWeights)
 readWeightsFromFile fname = do txt <- TextIO.readFile fname 
                                let Right result = parseCSV txt
                                let headers = map T.strip $ head result
@@ -89,6 +96,7 @@ readWeightsFromFile fname = do txt <- TextIO.readFile fname
                                                                                m
 
 -- | Compute sequence match score for a given pair of characters
+seqSim ::  SeqSimWeights -> Char -> Char -> Int
 seqSim (SeqSimWeights codes indices matrix) a b = matrix Repa.! (Z Repa.:. findInd a Repa.:. findInd b)
   where
     findInd x = if i >= 0
