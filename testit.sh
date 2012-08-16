170c56c8291197774f814ca98c5e9e42ad4a94f1
Michal J. Gajda <mjgajda@gmail.com>
2012-08-16 11:13:51 +0200

    Added a BMRB entry for ubiquitin.

--- testit.sh
+++ testit.sh
@@ -5,4 +5,6 @@
 #time ./Test3 data/seqsim.txt
 #time ./MakeDB smallest.str smallest_gap.str output.db
 #time ./MakeDB smallest.str largest.str output.db
-time ./SearchDB small.db K18_alone_shifty.csv
+#time ./SearchDB small.db K18_alone_shifty.csv
+time ./MakeDB ubq.str ubq.db
+time ./SearchDB ubq.db ubq.csv
