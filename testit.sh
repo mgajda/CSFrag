aa5f2b56e6e4ca61d592a7976e77953e04104b94
Michal J. Gajda <mjgajda@gmail.com>
2012-08-17 15:33:31 +0200

    Added a test with 4 ubiquitin entries and query.

--- testit.sh
+++ testit.sh
@@ -6,5 +6,7 @@
 #time ./MakeDB smallest.str smallest_gap.str output.db
 #time ./MakeDB smallest.str largest.str output.db
 #time ./SearchDB small.db K18_alone_shifty.csv
-time ./MakeDB ubq.str ubq.db
-time ./SearchDB ubq.db ubq.csv
+#time ./MakeDB ubq.str ubq.db
+#time ./SearchDB ubq.db ubq.csv
+#time ./MakeDB ubqs/*.str ubqs.db
+time ./SearchDB ubqs.db ubq.csv
