65c1e6e10fa0e354332c48d74fd65e37aa044e98
Michal J. Gajda <mgajda@gwdg.de>
2012-08-22 15:20:02 +0200

    Updated CheckSeq.hs to use Database.readDB.

--- CheckSeq.hs
+++ CheckSeq.hs
@@ -4,9 +4,10 @@ import Database
 
 import System.Environment(getArgs)
 import Control.Monad(forM)
+import qualified Data.Array.Repa as Repa
 
 main = do fnames <- getArgs
           forM fnames $ \fname ->
             do putStr $ fname ++ ": "
-               db <- decodeCompressedFile fname
-               print $ resArray (db :: Database)
+               db <- readDB fname
+               putStrLn . Repa.toList . resArray $ db
