f749e84a19c96973ea26c221d53b331378f03663
Michal J. Gajda <mjgajda@gmail.com>
2013-05-06 11:40:30 +0200

    CheckSeq: added help.

--- CheckSeq.hs
+++ CheckSeq.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
-import Database
 
-import System.Environment(getArgs)
-import Control.Monad(forM)
+import           System.IO(putStrLn)
+import           System.Environment(getArgs)
+import           Control.Monad     (forM, when)
 import qualified Data.Array.Repa as Repa
 
+import Database
+
 main = do fnames <- getArgs
+          when (null fnames) $ do
+            putStrLn "Usage: CheckSeq <input.str> ..."
+            putStrLn "Shows FASTA sequence read from NMR-STAR file."
           forM fnames $ \fname ->
             do putStr $ fname ++ ": "
                db <- readDB fname
                putStrLn . Repa.toList . resArray $ db
+
